c479917deecfbb1e85683bb3d5e5e8f2a28058b0
adorablenewbie <youngjong2965@gmail.com>
2025-08-14 14:32:43 +0900

    [ feat ] NPC 1차 작업

---
+++ Assets/Scripts/NPCJYJ/NPC.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour, IInteractable
+{
+
+    public string GetInteractPrompt()
+    {
+        //string str = $"{data.displayName}\n{data.description}";
+        //return str;
+        throw new System.NotImplementedException();
+    }
+
+    public void OnInteract()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
