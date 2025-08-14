3ccb6e8d3a1ac08a7017ff7149be44f1851dfd66
adorablenewbie <youngjong2965@gmail.com>
2025-08-14 12:07:23 +0900

    [ chore ] 작업장

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -106,7 +106,7 @@ public class PlayerController : MonoBehaviour
         cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
     }
 
-    private void RotateToCamera()
+    private void RotateToCamera() //씨네머신 때문에 생긴거~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     {
         // 전진 입력일 때만 회전 적용, 나중에 조금 손 보자
         if (curMovementInput.sqrMagnitude > 0.01f && curMovementInput.y > 0)
