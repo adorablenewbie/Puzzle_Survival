8fb6125092a96f851a44804703e846aad580ff18
totalpsh <66562462+totalpsh@users.noreply.github.com>
2025-08-16 18:19:29 +0900

    [ feat ] 건축 기능 구현, 앞으로 건축할 아이템들 추가

--- Assets/Scripts/Build/PreviewObject.cs
+++ Assets/Scripts/Build/PreviewObject.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class PreviewObject : MonoBehaviour
 {
-    // ㅇ
+    private List<Collider> colliderList = new List<Collider>();
 
     [SerializeField]public Vector3 cubeSize = Vector3.zero; // 큐브 크기
     public Color cubeColor = Color.green;  // 큐브 색상
 
+    private Renderer meshRenderer;
 
-    [SerializeField] private LayerMask grondLayer;
+    [SerializeField] private LayerMask groundLayer;
+    private const int IGNORE_RAYCAST_LAYER = 2;
 
     [SerializeField] private Material green;
     [SerializeField] private Material red;
@@ -19,18 +21,95 @@ public class PreviewObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        meshRenderer = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ChangeMaterial();
     }
 
-    private void OnDrawGizmosSelected()
+    private void ChangeMaterial()
     {
-        Gizmos.color = cubeColor;
-        Gizmos.DrawCube(transform.position, cubeSize); // 중심 위치, 크기
+        if(colliderList.Count > 0)
+        {
+            SetMaterial(red);
+        }
+        else
+        {
+            SetMaterial(green);
+        }
+    }
+
+    private void SetMaterial(Material mat)
+    {
+        //Material[] materials = new Material[GetComponent<MeshRenderer>().materials.Length];
+        //Material[] materials = new Material[meshRenderer.materials.Length];
+        //Debug.Log(meshRenderer.materials.Length);
+        //for (int i = 0; i < materials.Length; i++)
+        //{
+        //    materials[i] = mat;
+        //}
+
+        //meshRenderer.materials = materials;
+
+        meshRenderer.material = mat;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if(other.gameObject.layer != groundLayer && other.gameObject.layer != IGNORE_RAYCAST_LAYER)
+        if(((1 << other.gameObject.layer) & groundLayer) == 0 && other.gameObject.layer != IGNORE_RAYCAST_LAYER)
+            colliderList.Add(other);
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (((1 << other.gameObject.layer) & groundLayer) == 0 && other.gameObject.layer != IGNORE_RAYCAST_LAYER)
+            colliderList.Remove(other);
+
+    }
+
+
+    public bool IsBuildable()
+    {
+        return colliderList.Count == 0;
     }
 }
+
+
+// List가 있다. (배열)// 
+// List 길이를 50으로 해놓고
+// 생성을 하고 -> List에 Add
+// List의 길이 -> 지금 들어가 있는 오브젝트 개수 -> 지금 생성되어 있는 오브젝트의 개수
+// 50개까지만 생성을 하고 -> Add해준다.
+
+// Resource -> List
+
+// 따로따로 돌따로 나무따로 풀따로
+
+// 리소스매니저에서 해준다.
+// 플레이어가 오브젝트를 쳤어 그러면 Destroy() -> List에서 Remove
+// 현재 길이 49 List.Count가 50 이 안되면 50로 맞춰줘야함
+// -> Instantiate -> Add
+// -> 
+
+// 풀타입, 나무타입, 돌타입
+// 10, 10, 10
+// if(itemType == HarvertableType.Grass)
+// {
+//      for(int i = 0; i < 10; i++) // 10번
+//      {
+//          1개 생성한다.
+//          List.Add();
+//      }
+// }
+// 
+
+// Destroy -> 
+// 델리게이트 -> Destory -> ResourceManager에 있는 개수를 하나 줄여주는 메서드를 같이 실행시킨다.
+// 
+// 
--- Assets/Scripts/UI/UIBuild.cs
+++ Assets/Scripts/UI/UIBuild.cs
@@ -6,12 +6,24 @@ using UnityEngine.UI;
 
 public class UIBuild : MonoBehaviour
 {
-    bool isActive;
+    private bool isActive = false;
+    private bool isPreviewActive = false;
 
     [SerializeField] private GameObject baseUI;
     [SerializeField] private Button productionButton;
     [SerializeField] private Button buildButton;
-    [SerializeField] private Image listPanel;
+    [SerializeField] private Image productionListPanel;
+    [SerializeField] private Image buildListPanel;
+
+    [SerializeField] private Build[] builds;
+    private GameObject previewPrefab;
+    private GameObject installPrefab;
+
+    [SerializeField] private Transform player;
+
+    private RaycastHit hitInfo;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private float range;
 
     private void Awake()
     {
@@ -24,14 +36,47 @@ public class UIBuild : MonoBehaviour
     {
         isActive = false;
         baseUI.SetActive(isActive);
-        listPanel.gameObject.SetActive(false);
+        productionListPanel.gameObject.SetActive(false);
+        buildListPanel.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isPreviewActive) PreviewPositionUpdate();
 
+        if(Input.GetMouseButtonDown(0))
+        {
+            Build();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cancel();
+        }
     }
+
+    private void PreviewPositionUpdate()
+    {
+        if (Physics.Raycast(player.position, player.forward, out hitInfo, range, layerMask))
+        {
+            if(hitInfo.transform != null)
+            {
+                Vector3 location = hitInfo.point;
+                previewPrefab.transform.position = location;
+            }
+        }
+    }
+
+    public void SlotClick(int slotNumber)
+    {
+        previewPrefab = Instantiate(builds[slotNumber].previewPrefab, player.position + player.forward, Quaternion.identity);
+        installPrefab = builds[slotNumber].installPrefab;
+        isPreviewActive = true;
+        baseUI.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     public void UIControl(InputAction.CallbackContext context)
     {
         if(context.phase == InputActionPhase.Started)
@@ -59,17 +104,46 @@ public class UIBuild : MonoBehaviour
     {
         isActive = false;
         baseUI.SetActive(false);
+        productionListPanel.gameObject.SetActive(false);
+        buildListPanel.gameObject.SetActive(false);
         Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Cancel()
+    {
+        if (isPreviewActive)
+            Destroy(previewPrefab);
+
+        isActive = false;
+        isPreviewActive= false;
+        previewPrefab = null;
+        CloseUI();
+    }
+
+    private void Build()
+    {
+        if (isPreviewActive && previewPrefab.GetComponent<PreviewObject>().IsBuildable())
+        {
+            Instantiate(installPrefab, hitInfo.point, Quaternion.identity);
+            Destroy(previewPrefab);
+            isActive = false;
+            isPreviewActive = false;
+            previewPrefab = null;
+            installPrefab = null;
+        }
+
     }
 
     private void ProductionUIOpen()
     {
-        listPanel.gameObject?.SetActive(true);
+        buildListPanel.gameObject?.SetActive(false);
+        productionListPanel.gameObject?.SetActive(true);
         // 레시피 정보 넣어주기
     }
     private void BuildUIOpen()
     {
-        listPanel.gameObject?.SetActive(true);
+        productionListPanel.gameObject?.SetActive(false);
+        buildListPanel.gameObject?.SetActive(true);
         // 레시피 정보 넣어주기
 
     }
