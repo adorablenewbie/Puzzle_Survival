ffe1c9548bb90714808e802060af9e6e84024a32
bABCDd <rlatkdrbs6845@gmail.com>
2025-08-18 16:48:45 +0900

    [feat] 아이템 제작 메서드

--- Assets/2. Scripts/UI/PSH&KSG/UIProduction.cs
+++ Assets/2. Scripts/UI/PSH&KSG/UIProduction.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UIProduction : MonoBehaviour
@@ -10,42 +8,58 @@ public class UIProduction : MonoBehaviour
     public GameObject ProductionAxeButton;
     public GameObject ProductionSwordButton;
 
-    // Start is called before the first frame update
+    // 인벤토리 / 제작 아이템 연결
+    public UIInventory uiInventory;
+    public ItemData soupItem;
+    public ItemData potionItem;
+    public ItemData axeItem;
+    public ItemData swordItem;
+
     void Start()
     {
         gameObject.SetActive(false);
+        if (uiInventory == null) uiInventory = FindObjectOfType<UIInventory>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Debug.Log($"UIProduction {itemslots.Length}");
+       // Debug.Log($"UIProduction {itemslots.Length}");
     }
 
-    public void OpenProductionList()
-    {
-        gameObject.SetActive(true);
-    }
-
-    public void CloseProductionList()
-    {
-        gameObject.SetActive(false);
-    }
+    public void OpenProductionList() { gameObject.SetActive(true); }
+    public void CloseProductionList() { gameObject.SetActive(false); }
 
     public void OnProductionSoupButton()
     {
-
+        if (uiInventory == null || soupItem == null) { Debug.LogWarning("Soup 제작 세팅 누락"); return; }
+        PlayerManager.Instance.Player.itemData = soupItem;
+        uiInventory.AddItem();
+        Debug.Log($"{soupItem.displayName} 제작 완료!");
     }
+
     public void OnProductionPotionButton()
     {
-
+        if (uiInventory == null || potionItem == null) { Debug.LogWarning("Potion 제작 세팅 누락"); return; }
+        PlayerManager.Instance.Player.itemData = potionItem;
+        uiInventory.AddItem();
+        Debug.Log($"{potionItem.displayName} 제작 완료!");
     }
+
     public void OnProductionAxeButton()
     {
-
+        if (uiInventory == null || axeItem == null) { Debug.LogWarning("Axe 제작 세팅 누락"); return; }
+        PlayerManager.Instance.Player.itemData = axeItem;
+        uiInventory.AddItem();
+        Debug.Log($"{axeItem.displayName} 제작 완료!");
     }
+
     public void OnProductionSwordButton()
     {
-
+        if (uiInventory == null || swordItem == null) { Debug.LogWarning("Sword 제작 세팅 누락"); return; }
+        PlayerManager.Instance.Player.itemData = swordItem;
+        uiInventory.AddItem();
+        Debug.Log($"{swordItem.displayName} 제작 완료!");
     }
 }
+
+
