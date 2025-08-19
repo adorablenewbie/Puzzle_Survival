6d9efb19abc994538e382947b7ff1ad8139c8271
totalpsh <66562462+totalpsh@users.noreply.github.com>
2025-08-19 11:42:37 +0900

    [ fix ] 제작&건축 UI main씬에 병합 및 닫기 로직 수정

--- Assets/2. Scripts/UI/PSH&KSG/CraftUIManager.cs
+++ Assets/2. Scripts/UI/PSH&KSG/CraftUIManager.cs
@@ -73,6 +73,8 @@ public class CraftUIManager : MonoBehaviour
     {
         isCraftActive = false;
         craft.CloseUI();
+        build.CloseBuildList();
+        production.CloseProductionList();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
